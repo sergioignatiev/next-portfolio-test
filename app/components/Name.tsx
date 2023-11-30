aeb5c83a2fde157d7f45f9c517cab544743a701c
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-30 23:38:03 +0300

    fall

--- app/components/Name.tsx
+++ app/components/Name.tsx
@@ -1,14 +1,29 @@
 'use client'
-import { InViev } from "../lib/inviev"
+import { InViev,InVievLeft } from "../lib/inviev"
 export function Name(){
-    return   <div className="relative"><h1 className='text-[100px] font-semibold  flex'>
+    return   <><div className="relative"><h1 className='text-[100px] font-semibold  flex'>
   <InViev duration={0.1}>  <div className="" > Сергей</div></InViev>
     <InViev duration={0.5}><div className="ml-[30px] text-[red]">Игнатьев</div></InViev>
   </h1>
-  <h1 className='text-[100px] font-semibold  flex'>
-  <InViev duration={0.8}>  <div className="text-[red]"> WEB</div></InViev>
-    <InViev duration={1.2}><div className="ml-[30px]">Developer</div></InViev>
-  </h1>
-  
+
+
+
   </div>
+  <h1 className='text-[100px] font-semibold  flex'>
+  <InVievLeft duration={0.1}>  <div className="text-[red]"> W</div></InVievLeft>
+    <InVievLeft duration={0.2}><div className="text-[red]">E</div></InVievLeft>
+    <InVievLeft duration={0.3}><div className="">B</div></InVievLeft>
+    <InVievLeft duration={0.4}>  <div className=""> -</div></InVievLeft>
+    <InVievLeft duration={0.5}><div className="text-[red]">D</div></InVievLeft>
+    <InVievLeft duration={0.6}><div className="text-[red]">E</div></InVievLeft>
+    <InVievLeft duration={0.7}><div className="">V</div></InVievLeft>
+    <InVievLeft duration={0.8}><div className="">E</div></InVievLeft>
+    <InVievLeft duration={0.9}><div className="">L</div></InVievLeft>
+    <InVievLeft duration={1}>  <div className="">O</div></InVievLeft>
+    <InVievLeft duration={1.1}><div className="">P</div></InVievLeft>
+    <InVievLeft duration={1.2}><div className="">E</div></InVievLeft>
+    <InVievLeft duration={1.3}><div className="">R</div></InVievLeft>
+
+  </h1>
+</>
 }
--- app/lib/inviev.tsx
+++ app/lib/inviev.tsx
@@ -34,3 +34,31 @@ export function InViev({children,width='fit-content',duration=0.4}:Props) {
  
   </div>)
 }
+export function InVievLeft({children,width='fit-content',duration=0.4}:Props) {
+  const ref =useRef(null)
+  const isInView=useInView(ref,{once:false})
+  const mainControls=useAnimation()
+  useEffect(()=>{
+    if(isInView){
+     mainControls.start('visible')
+    }
+    else if(!isInView){
+     mainControls.start('hidden')
+    }
+  },[isInView])
+   return (
+     <div ref={ref}>
+     
+     <motion.div className="py-[20px]" 
+     variants={{
+         hidden:{transform:"translateY(-1900px)"},
+         visible:{transform:'translateY(0px)'}
+ 
+     }}
+     initial='hidden'
+     animate={mainControls}
+     transition={{duration:0.4,delay:duration}}
+     >{children}</motion.div>  
+  
+   </div>)
+ }
